3bceda4df71092dcd8fb0123383f406f3f99dbbc
mackliu <macklun@gmail.com>
2021-07-05 11:13:12 +0800

    使用ajax請求api查詢班級資料

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,5 +1,9 @@
 <?php
 //處理查詢資料的請求
+include_once 'base.php';
 
+$classNum=$_GET['classNum'];
+$students=$Stu->all(" where substring(`class_num`,1,3)='$classNum'") ;
+echo json_encode($students);
 
 ?>
